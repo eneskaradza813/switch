8aaa3958cdc16ec0e1e52f9e563d81fb340a5e08
enes <>
2019-12-28 14:01:20 +0100

    i123 switch
    = upotreba switch

---
+++ src/Vjzeba1.java
@@ -0,0 +1,16 @@
+
+
+public class Vjzeba1 {
+
+    public static void main(String[] args) {
+
+        
+        Calculator c = new Calculator();
+        
+        c.operand1 = 2;
+        c.operand2 = 5;
+        c.operator = '+';
+        System.out.println(c.calculate());
+    }
+
+}
